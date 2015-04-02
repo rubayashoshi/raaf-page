b0c7ecd41bfb64a934cd0cca325678efd1e84ad9
Rubaya Shoshi <rubayashoshi@gmail.com>
2015-04-03 00:46:27 +0100

    Added simple for for uploading add

--- src/RAAFPAGE/AdBundle/Controller/AdController.php
+++ src/RAAFPAGE/AdBundle/Controller/AdController.php
@@ -2,6 +2,8 @@
 
 namespace RAAFPAGE\AdBundle\Controller;
 
+use RAAFPAGE\AdBundle\Entity\Property;
+use RAAFPAGE\AdBundle\Form\Type\PropertyType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -18,11 +20,24 @@ class AdController extends Controller
     }
 
     /**
-     * @Route("/seller/add/upload")
+     * @Route("/seller/add/upload", name="upload_add")
      * @Template()
      */
     public function uploadAction()
     {
-        return array();
+        $property = new Property();
+        $form = $this->createForm(new PropertyType(), $property);
+        $form->handleRequest($this->getRequest());
+
+        if ($this->getRequest()->isMethod('POST')) {
+            if ($form->isValid()) {
+                $property = $form->getData();
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($property);
+                $manager->flush();
+            }
+        }
+
+        return array('form' => $form->createView());
     }
 }
